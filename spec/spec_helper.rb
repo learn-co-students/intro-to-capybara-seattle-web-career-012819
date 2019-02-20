b00b4c1c5142ce645353461721895e248aeaa3ee
omfgtora <spaminol@gmail.com>
2019-02-19 17:43:46 -0800

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,20 +1,29 @@
 # Load RSpec and Capybara
-require 'rspec'
-require 'capybara/rspec'
-require 'capybara/dsl'
+require "rspec"
+require "capybara/rspec"
+require "capybara/dsl"
 
 # Configure RSpec
 RSpec.configure do |config|
   # Mixin the Capybara functionality into Rspec
   config.include Capybara::DSL
-  config.order = 'default'
+  config.order = "default"
 end
 
 # Define the application we're testing
 def app
   # Load the application defined in config.ru
-  Rack::Builder.parse_file('config.ru').first
+  Rack::Builder.parse_file("config.ru").first
 end
 
 # Configure Capybara to test against the application above.
 Capybara.app = app
+
+require "spec_helper"
+
+describe "GET '/' - Greeting Form" do
+  it "welcomes the user" do
+    visit "/"
+    expect(page.body).to include("Welcome!")
+  end
+end
