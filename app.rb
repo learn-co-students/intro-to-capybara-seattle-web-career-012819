9f86c9f35aeda07724b7e8285a9d8c40d0eddeed
github username <github email address>
2019-02-19 18:39:35 -0800

    First commit.

--- app.rb
+++ app.rb
@@ -1,4 +1,9 @@
 class Application < Sinatra::Base
-  # Write your code here!
+  get ('/') {
+    erb :index
+  }
 
+  post ('/greet') {
+    erb :greet
+  }
 end
--- spec/application_integration_spec.rb
+++ spec/application_integration_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "GET '/' - Greeting Form" do
-  # Code from previous example
+ # Code from previous example
   it 'welcomes the user' do
     visit '/'
     expect(page.body).to include("Welcome!")
